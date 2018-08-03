c8317cb9ca8fcb2352cce594810ba8bd3589a6a1
Rafal Maksymczuk <rafal.maksymczuk@ds-consultants.eu>
2018-08-03 08:42:53 +0200

    Fix for tests

--- src/dcgSelenium/Pages/PreviewPage.java
+++ src/dcgSelenium/Pages/PreviewPage.java
@@ -1,9 +1,11 @@
 package dcgSelenium.Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,6 +14,7 @@ public class PreviewPage extends SettingsPage {
 
 	public PreviewPage(WebDriver driver, WebDriverWait wait) {
 		super(driver, wait);
+		PageFactory.initElements(driver, this);
 	}
 
 	private static final String actualNameLocator = "//*[@id='inline-header-name']/div[1]/a[1]";
