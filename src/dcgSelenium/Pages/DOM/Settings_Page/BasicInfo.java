dea9b7b1329e9a4a33892ba2a97d4fb709f24f36
artursuchowierski <artursuchowierski@macbook-pro-artur.home>
2018-08-10 12:54:29 +0200

    small refactor, building tests with maven, parallel test execution

---
+++ src/dcgSelenium/Pages/DOM/Settings_Page/BasicInfo.java
@@ -0,0 +1,31 @@
+package dcgSelenium.Pages.DOM.Settings_Page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import dcgSelenium.Pages.SettingsPage;
+
+public class BasicInfo extends SettingsPage{
+	
+	public static String nameid = "name";
+	public static String titlechckid = "title";
+	public static String professionalExpid = "professionalExpectations";
+	public static String personalNoteid = "personalNote";
+	
+	// expected basic info
+	public static String expectedName = "Jan Kowalski";
+	public static String expectedTitle = "QA Consultant";
+	public static String expectedExp = "test 123456789";
+	public static String expectedNote = "test personal note 12345";
+
+	public BasicInfo(WebDriver driver, WebDriverWait wait) {
+		super(driver, wait);
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	
+
+}
---
+++ src/dcgSelenium/Pages/DOM/Settings_Page/Skills.java
@@ -0,0 +1,32 @@
+package dcgSelenium.Pages.DOM.Settings_Page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import dcgSelenium.Pages.SettingsPage;
+
+public class Skills extends SettingsPage{
+
+	// skills
+	public static String skillsetAddBtn = "skillset-add-button";
+	public static String skillName = "newSkillset";
+	public static String skillSaveBtn = "skillset-save-button";
+	public static String newTagField = "//tag-input-form[@class='ng-tns-c5-4']/form[1]/input[1]";
+
+	// expected skills
+	public static String expectedSkillName = "My skills";
+	public static String expectedTags = "Selenium, Python, Appium";
+
+	public static String removeExpBtn = "experience-remove-button";
+	public static String removeEduBtn = "education-remove-button";
+	public static String removeSkillBtn = "skillset-my-skills-remove-button";
+	public static String removeProjBtn = "project-remove-button";
+	public static String removeMainProjBtn = "main-project-remove-button";
+	public static String newHireBtn = "new-hire-button";
+
+	public Skills(WebDriver driver, WebDriverWait wait) {
+		super(driver, wait);
+		// TODO Auto-generated constructor stub
+	}
+
+}
