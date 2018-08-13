6a6d4b5b2deae52afffddec10e843303bc30f967
artursuchowierski <artursuchowierski@macbook-pro-artur.home>
2018-08-13 09:54:05 +0200

    added DriverSetup interface and DriverType enum

--- src/dcgDrivers/DriverFactory.java
+++ src/dcgDrivers/DriverFactory.java
@@ -1,31 +1,53 @@
 package dcgDrivers;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import static dcgDrivers.DriverType.CHROME;
+
+import static dcgDrivers.DriverType.valueOf;
 
 public class DriverFactory {
 
 	private RemoteWebDriver webDriver;
+	private DriverType selectedDriverType;
 	private final String operatingSystem = System.getProperty("os.name").toUpperCase();
 	private final String systemArchitecture = System.getProperty("os.arch");
 
-	RemoteWebDriver getDriver() {
+	public DriverFactory() {
+	DriverType driverType = CHROME;
+	String browser = System.getProperty("browser",
+	driverType.toString()).toUpperCase();
+	try {
+	driverType = valueOf(browser);
+	} catch (IllegalArgumentException ignored) {
+	System.err.println("Unknown driver specified, defaulting to '" + driverType + "'...");
+	} catch (NullPointerException ignored) {
+	System.err.println("No driver specified, defaulting to '" + driverType + "'...");
+	}selectedDriverType = driverType;
+	}
+
+	public RemoteWebDriver getDriver() {
 		if (null == webDriver) {
-			System.out.println(" ");
-			System.out.println("Current Operating System: " + operatingSystem);
-			System.out.println("Current Architecture: " + systemArchitecture);
-			System.out.println("Current Browser Selection: Chrome");
-			System.out.println(" ");
-			webDriver = new ChromeDriver();
+			instantiateWebDriver(selectedDriverType);
 		}
 		return webDriver;
 	}
 
-	void quitDriver() {
+	public void quitDriver() {
 		if (null != webDriver) {
 			webDriver.quit();
 			webDriver = null;
 		}
+	}
+
+	private void instantiateWebDriver(DriverType driverType) {
+		System.out.println(" ");
+		System.out.println("Local Operating System: " + operatingSystem);
+		System.out.println("Local Architecture: " + systemArchitecture);
+		System.out.println("Selected Browser: " + selectedDriverType);
+		System.out.println(" ");
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+		webDriver = driverType.getWebDriverObject(desiredCapabilities);
 	}
 
 }
